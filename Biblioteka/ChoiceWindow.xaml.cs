00c464ac49361eb652b7228ae29f8499d902020c
Rafal <pasonrafal64@gmail.com>
2025-03-14 14:02:45 +0100

    first_commit

---
+++ Biblioteka/ChoiceWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Biblioteka
+{
+    public partial class ChoiceWindow : Window
+    {
+        public ChoiceWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            // okno dodawania uzytkownika
+            AddUserWindow addUserWindow = new AddUserWindow();
+            addUserWindow.Owner = this.Owner; // przekazujemy wlasciciela (glowne okno)
+            addUserWindow.Show();
+            this.Close(); // zamykamy okno dialogowe
+        }
+
+        private void btnAddExemplar_Click(object sender, RoutedEventArgs e)
+        {
+            // okno dodawania egzemplarza
+            AddExemplarWindow addExemplarWindow = new AddExemplarWindow();
+            addExemplarWindow.Owner = this.Owner;
+            addExemplarWindow.Show();
+            this.Close();
+        }
+    }
+
+}
---
+++ Biblioteka/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Biblioteka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Background = new SolidColorBrush(Colors.White);
+        }
+
+        private bool isDark = false;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isDark)
+            {
+                this.Background = new SolidColorBrush(Colors.DarkGray);
+            }
+            else
+            {
+                this.Background = new SolidColorBrush(Colors.White);
+            }
+
+            isDark = !isDark;
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            ChoiceWindow add = new ChoiceWindow();
+            add.ShowDialog();
+        }
+    }
+}
