00fc7f93b61845ba729f045b7fe7136059748cc3
Rafal <pasonrafal64@gmail.com>
2025-04-04 14:28:03 +0200

    ReturnWindow

--- Biblioteka/ReturnWindow.xaml.cs
+++ Biblioteka/ReturnWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,20 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
-using System.Xml.Serialization;
 
 namespace Biblioteka
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logika interakcji dla klasy ReturnWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ReturnWindow : Window
     {
         private IImageBackgroundChanger backgroundChanger;
-        public MainWindow()
+        public ReturnWindow()
         {
             InitializeComponent();
 
@@ -36,24 +36,6 @@ namespace Biblioteka
             backgroundChanger.SetInterval(5);
 
             backgroundChanger.StartChanging(Background);
-        }
-
-        private void add_button_click(object sender, RoutedEventArgs e)
-        {
-            ChoiceWindow add = new ChoiceWindow();
-            add.ShowDialog();
-        }
-
-        private void borrow_button_click(object sender, RoutedEventArgs e)
-        {
-            BorrowWindow add = new BorrowWindow();
-            add.ShowDialog();
-        }
-
-        private void return_button_click(object sender, RoutedEventArgs e)
-        {
-            ReturnWindow add = new ReturnWindow();
-            add.ShowDialog();
         }
     }
 }
