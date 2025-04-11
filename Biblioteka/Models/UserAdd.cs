d86db3e0037c921860912eee869524b1c5118761
Rafal <pasonrafal64@gmail.com>
2025-04-11 14:41:52 +0200

    11.04.2025

---
+++ Biblioteka/Models/UserAdd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka.Models
+{
+    class UserAdd
+    {
+        public int ID { get; set; }
+
+        public string E_mail { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
